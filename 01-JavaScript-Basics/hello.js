9ebc070f3e11dcc9e817137efbe70c274e34d63b
Rem Simiyu <remsimiyu@gmail.com>
2025-07-18 17:06:38 +0300

    JavaScript Full Course

---
+++ 01-JavaScript-Basics/hello.js
@@ -0,0 +1,12 @@
+// alert("Hello, World!");
+console.log("Hello, World!");
+let js = "amazing";
+
+if (js === "amazing") console.log("JavaScript is FUN!");
+
+js = "boring";
+if (js === "boring") console.log("JavaScript is BORING!");
+
+console.log(40 + 8 + 23 - 10);
+
+document.body.innerHTML="hello";
---
+++ 01-JavaScript-Basics/lesson_1_exercise.js
@@ -0,0 +1,40 @@
+// 1a. Use alert(...) to display "Good morning!" in a popup.
+// alert("Good morning!");
+
+// 1b. Display your name in a popup.
+// alert("Rem Simiyu");
+
+// 1c. Use math to calculate 10 + 5 in the console.
+console.log(10 + 5);
+
+// 1d. Calculate 20 - 5 in the console.
+console.log(20 - 5);
+
+// 1e. Calculate 2 + 2 - 5 in the console.
+console.log(2 + 2 - 5);
+
+// 1f. Use document.body.innerHTML= ...; to display "Good morning!" on the web page.
+document.body.innerHTML= "Good morning!"
+
+// 1g. Display your name on the web page.
+document.body.innerHTML = "Rem Simiyu"
+
+// 1h. You order a T-shirt for $10, socks for $8, and dinner plates for $20. use JavaScript to calculate the total cost of your order.
+const tshirt = 10;
+const socks = 8;
+const dinnerplates = 20;
+const totalAmount = tshirt + socks + dinnerplates;
+
+console.log(totalAmount);
+
+// 1i. Your bank account has $100, you spend $20 on lunch, $50 on dinner, and earn $200 from your job. Calculate how much money you have.
+const bankAccount = 100;
+const lunch = 20;
+const dinner = 50;
+const earning = 200;
+
+const totalMoney = bankAccount - lunch - dinner;
+console.log(earning - totalMoney);
+
+// 1i. Use document.body.innerHTML=...; to make the web page blank.
+document.body.innerHTML= "";
