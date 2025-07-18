f500e12a77b7c7abb99f469d11bb098eef07c7af
Rem Simiyu <remsimiyu@gmail.com>
2025-07-18 17:35:04 +0300

    JavaScript Full Course

---
+++ 02-Numbers_Math/numbers_&_math.js
@@ -0,0 +1,37 @@
+// Addition
+console.log(2 + 2);
+
+// Subtraction
+console.log(10 - 5);
+
+// Multiplication
+console.log(10 * 3);
+
+// Division
+console.log(10 / 2);
+
+console.log(2 + 2 + 2);
+console.log(2.2 + 2.2);
+console.log( 10.90 * 2 + 20.95);
+console.log( 10.90 * 2 + 20.95 + 4.99);
+
+// Operation Precedence
+// * / are done first.
+// + - are done after.
+// We can use () brackets to control which part gets done first.
+/* 
+Order of Operation
+1. (...)
+2. * /
+3. + -
+*/
+
+console.log( 1 + 1 * 3); // 1 * 3 is done first then the 3 + 1 is done after which is equal to 4.
+console.log( (1 + 1) * 3); // 1 + 1 is done first then the 2 * 3 which is equal to 6 is done after.
+
+console.log(20.95 + 7.99 * 2);
+console.log(36.93 * 0.1);
+
+console.log((20.95 + 7.99 * 2) * 0.1);
+
+
